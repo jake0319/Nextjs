9219634efce759897cfee9d8853f102267af46fd
Jake shim <shjun1411@naver.com>
2023-10-31 23:20:47 +0900

    feat: page move, URL obeject

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ export default function Home() {
     <>    
     <Link href={"/search"}>Search페이지로 이동</Link>
     <Link href={`/country/${code}`}>템플릿 리터럴 방식으로 이동</Link>
+    <Link href={{
+      pathname: '/country/[code]', 
+      query: {code: code}
+    }}>URL obejct형식으로 이동(좀 더구조화된 방식)</Link>
     </>
   )
 }
